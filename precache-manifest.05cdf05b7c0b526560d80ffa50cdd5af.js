166a0a2ddc35213ec0b0fac1ea5f29b8b5d95029
Abdallah Sayed <34820902+abdallah-95@users.noreply.github.com>
2020-09-04 19:35:04 +0200

    Updates

---
+++ precache-manifest.05cdf05b7c0b526560d80ffa50cdd5af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb9eeac51243b098826bae48d0571e6",
+    "url": "/movie-info-app/index.html"
+  },
+  {
+    "revision": "8784afb1fb2fe46f824b",
+    "url": "/movie-info-app/static/css/2.b80483f4.chunk.css"
+  },
+  {
+    "revision": "8ed9a45449fcd7ea5baf",
+    "url": "/movie-info-app/static/css/main.58ed348b.chunk.css"
+  },
+  {
+    "revision": "8784afb1fb2fe46f824b",
+    "url": "/movie-info-app/static/js/2.cdf99813.chunk.js"
+  },
+  {
+    "revision": "e92311d493a1dde02c3c18735aca81e5",
+    "url": "/movie-info-app/static/js/2.cdf99813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ed9a45449fcd7ea5baf",
+    "url": "/movie-info-app/static/js/main.a03e5a59.chunk.js"
+  },
+  {
+    "revision": "41e0599217bdab1d5fb9",
+    "url": "/movie-info-app/static/js/runtime-main.3ad5e0cb.js"
+  },
+  {
+    "revision": "2cdc8609a5ef81f70ccafeec6a4ec698",
+    "url": "/movie-info-app/static/media/17580.2cdc8609.jpg"
+  }
+]);
